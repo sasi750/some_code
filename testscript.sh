c46541aaedd14cc8f86863edd331b30aef1b6d60
sasi750 <52458996+sasi750@users.noreply.github.com>
2020-04-20 16:18:08 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "This is an examle of a script"
-echo "This is a param $PASSED"
-NUMBER=$(($FIRSTNUMBER + $SECONDNUMBER)) 
+NUMBER=$(($NUMBERONE + $NUMBERTWO)) 
 echo "$NUMBER"
 echo "This is COMPLETE"
